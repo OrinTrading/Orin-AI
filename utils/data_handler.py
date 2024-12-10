ad0a75e0a3ae0ee9c3ebafd1d8e1d3c53edd51f8
OrinTrading <kehnyth132005@gmail.com>
2024-12-10 04:34:38 -0700

    Update data_handler.py

--- utils/data_handler.py
+++ utils/data_handler.py
@@ -1,1 +1,13 @@
+import json
+
+class DataHandler:
+    @staticmethod
+    def load_user_profiles(file_path):
+        with open(file_path, 'r') as file:
+            return json.load(file)
+
+    @staticmethod
+    def save_user_profiles(file_path, data):
+        with open(file_path, 'w') as file:
+            json.dump(data, file, indent=4)
 
