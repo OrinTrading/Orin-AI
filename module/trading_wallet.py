301327e008ae96223feea733549dddeea5e32547
OrinTrading <kehnyth132005@gmail.com>
2024-12-10 04:33:40 -0700

    Update trading_wallet.py

--- module/trading_wallet.py
+++ module/trading_wallet.py
@@ -1,1 +1,15 @@
+class Wallet:
+    def __init__(self, initial_funds=0):
+        self.balance = initial_funds
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            return amount
+        else:
+            print("Insufficient balance!")
+            return 0
 
