6b3862f13a9547389d3ab39bc6d01afdacac1338
OrinTrading <kehnyth132005@gmail.com>
2024-12-10 04:34:19 -0700

    Update risk_management.py

--- module/risk_management.py
+++ module/risk_management.py
@@ -1,1 +1,10 @@
+class RiskManager:
+    def __init__(self, balance, risk_tolerance=0.1):
+        self.balance = balance
+        self.risk_tolerance = risk_tolerance
 
+    def calculate_max_risk(self):
+        return self.balance * self.risk_tolerance
+
+    def is_within_risk_limit(self, trade_amount):
+        return trade_amount <= self.calculate_max_risk()
